0104c1079264154575ff1113f1650aa1af8edc6f
Gustavo <gstvramalho@gmail.com>
2019-03-03 15:24:44 -0300

    Some changes

--- src/main/java/br/gov/sp/fatec/SpringDataJpaApplication.java
+++ src/main/java/br/gov/sp/fatec/SpringDataJpaApplication.java
@@ -5,25 +5,33 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.gov.sp.fatec.service.UsuarioService;
+import br.gov.sp.fatec.model.Livro;
+import br.gov.sp.fatec.service.PessoaService;
+
+//import br.gov.sp.fatec.service.UsuarioService;
 
 @SpringBootApplication
 public class SpringDataJpaApplication implements CommandLineRunner {
 
 	@Autowired
-	private UsuarioService usuarioService;
+	private PessoaService pessoaService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJpaApplication.class, args);
 	}
 
-	public void setUsuarioService(UsuarioService usuarioService) {
-		this.usuarioService = usuarioService;
-	}
-
 	public void run(String... args) throws Exception {
-		// usuarioService.incluirUsuario("Teste JPA", "alguma senha",
-		// "Autorização nenhuma");
-//		usuarioService.excluirUsuario("Teste JPA");
+		// pessoaService.incluirPessoa("gustavo", "37225355813", "M",
+		// "Solteiro",
+		// "Jacarei");
+
+		// pessoaService.adicionarLivro("gustavo", "Goose bumps",
+		// "Desconhecido", "Livro de terror", "Terror");
+		// System.out.println("Adicionado com sucesso!");
+
+		Livro livro = pessoaService.buscarLivroByNome("Goose bumps");
+		System.out.println("Nome do livro procurado : " + livro.getNome()
+				+ " do autor : " + livro.getAutor());
+
 	}
 }
