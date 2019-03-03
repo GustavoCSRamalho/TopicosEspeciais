fde8fc753819bd45869bf2e32714f82c2ec4976a
Gustavo <gstvramalho@gmail.com>
2019-03-03 11:00:56 -0300

    Project working

--- src/main/java/org/gustavo/modelos/Pessoa.java
+++
@@ -1,9 +0,0 @@
-package org.gustavo.modelos;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-
-public abstract class Pessoa {
-
-}
